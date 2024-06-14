4d2301ee1dbb00241d431052bde6211d0039b02f
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-14 13:33:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function dayOfAYear(year){
+	
+		if(year%100===0){
+			if(year%400===0){
+				return 366;
+			}
+			else{
+				return 365;
+			}
+		}
+	    else{
+			if(year%4===0){
+				return 366;
+			}
+			else{
+				return 365;
+			}
+		}
+}
+console.log(dayOfAYear(year));
+	
+}
